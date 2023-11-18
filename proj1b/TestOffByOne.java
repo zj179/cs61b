01225b452864dc6c1414ca80e69b7f75e4da6abc
zj179 <150653562+zj179@users.noreply.github.com>
2023-11-18 17:47:36 +0800

    Add files via upload

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -8,7 +8,15 @@ public class TestOffByOne {
     static CharacterComparator offByOne = new OffByOne();
     @Test
     public void isequalChars(){
-        assertTrue(offByOne.equalChars('&','%'));
+
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('A', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
     }
 
     // Your tests go here.
